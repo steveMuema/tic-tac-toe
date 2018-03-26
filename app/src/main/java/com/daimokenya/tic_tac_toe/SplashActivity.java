83bc997e8cff07e0dd64090dc1dda879179830f1
hitchs3x <hitchs3x@gmail.com>
2018-03-26 20:11:58 +0300

    [Feature #156266984] Add application Splash Screen

---
+++ app/src/main/java/com/daimokenya/tic_tac_toe/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.daimokenya.tic_tac_toe;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //set a new intent to open an activity
+        Intent splash_intent = new Intent(this, MainActivity.class);
+        startActivity(splash_intent);
+        finish();
+    }
+}
